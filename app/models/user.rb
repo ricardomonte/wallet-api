7128c78141b7267fded90ee38fed7c5451d75ccb
Ricardo Montenegro <ramn21@gmail.com>
2021-08-13 17:14:46 +0200

    Generate user model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_secure_password
+  has_one :account, class_name: "account", foreign_key: "account_id", dependent: :delete
+  has_many :transactions, dependent: :destroy
+  validates_presence_of :name, :lastname, :email, :password_digest
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates_format_of :email, with: VALID_EMAIL_REGEX
+end

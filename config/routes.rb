444a2f0525cea84c6560fd9c77a9c0dbbc0282c2
Ricardo Montenegro <ramn21@gmail.com>
2021-08-13 17:15:32 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      post '/sign_up', to: "registrations#create"
+      post '/sign_in', to: "sessions#create"
+      delete '/sign_out', to: "sessions#destroy"
+      resources :accounts, only: [:index]
+      resources :transactions, only: [:index, :create]
+    end
+  end
 end
